8eb114807ab86762c13b6b8c4722d7204e3fe20a
Claudio Colucci <81558771+Cloudus91@users.noreply.github.com>
2021-10-12 14:45:48 +0200

    initial Author route

--- server.js
+++ server.js
@@ -7,7 +7,8 @@ const app = express();
 const expressLayouts = require("express-ejs-layouts");
 const path = require("path");
 
-const indexRouter = require("./routes/index")
+const indexRouter = require("./routes/index");
+const authorRouter = require("./routes/author")
 
 app.set("view engine", "ejs");
 
@@ -15,6 +16,7 @@ app.set("views", path.join(__dirname, "views"));
 app.set("layout", "layouts/layout");
 app.use(expressLayouts);
 app.use(express.static("public"))
+app.use(express.urlencoded({ extended: true }));
 
 const mongoose = require("mongoose");
 mongoose.connect(process.env.DATABASE_URL)
@@ -24,5 +26,7 @@ db.on("error", error => console.error(error))
 db.once("open", error => console.log("Connected to Mongoose"))
 
 app.use("/", indexRouter)
+app.use("/authors", authorRouter)
+
 
 app.listen(process.env.PORT || 3000)
