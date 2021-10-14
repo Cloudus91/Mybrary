381a20fdda1f1810eb3b5673f09ed43084db4f41
Claudio Colucci <81558771+Cloudus91@users.noreply.github.com>
2021-10-14 21:55:43 +0200

    File Upload Database

--- routes/books.js
+++ routes/books.js
@@ -1,22 +1,12 @@
 const express = require("express");
 const router = express.Router();
-const multer = require("multer");
-const path = require("path");
-const fs = require("fs")
 const Book = require("../models/book");
-const uploadPath = path.join("public", Book.coverImageBasePath)
 const Author = require("../models/author");
-const imageMimeType = ["image/jpeg", "image/png", "image/gif"]
-const upload = multer({
-    dest: uploadPath,
-    fileFilter: (req, file, callback) => {
-        callback(null, imageMimeType.includes(file.mimetype))
-    }
-})
+const imageMimeTypes = ["image/jpeg", "image/png", "image/gif"]
 
 // All Book Route
 router.get("/", async (req, res) => {
-    const query = Book.find({})
+    let query = Book.find({})
     if (req.query.title != null && req.query.title != "") {
         query = query.regex("title", new RegExp(req.query.title, "i"))
     }
@@ -47,34 +37,26 @@ router.get("/new", async (req, res) => {
 
 
 // Create Book Route
-router.post("/", upload.single("cover"), async (req, res) => {
-    const fileName = req.file != null ? req.file.filename : null
+router.post("/", async (req, res) => {
     const book = new Book({
         title: req.body.title,
         author: req.body.author,
         publishDate: new Date(req.body.publishDate),
         pageCount: req.body.pageCount,
-        coverImageName: fileName,
         description: req.body.description
     })
+    saveCover(book, req.body.cover)
     try {
         const newBook = await book.save();
         // res.redirect(`books/${newBook.id}`);
         res.redirect("books");
     }
     catch {
-        if (book.coverImageName != null) {
-            removeBookCover(book.coverImageName)
-        }
         renderNewPage(res, book, true)
     }
+
 })
 
-function removeBookCover(fileName) {
-    fs.unlink(path.join(uploadPath, fileName), err => {
-        if (err) console.err(err)
-    })
-}
 
 async function renderNewPage(res, book, hasError = false) {
     try {
@@ -88,6 +70,15 @@ async function renderNewPage(res, book, hasError = false) {
     }
     catch {
         res.redirect("/books")
+    }
+}
+
+function saveCover(book, coverEncoded) {
+    if (coverEncoded == null) return
+    const cover = JSON.parse(coverEncoded)
+    if (cover != null && imageMimeTypes.includes(cover.type)) {
+        book.coverImage = new Buffer.from(cover.data, "base64")
+        book.coverImageType = cover.type
     }
 }
 
--- server.js
+++ server.js
@@ -5,26 +5,25 @@ if (process.env.NODE_ENV !== "production") {
 const express = require("express");
 const app = express();
 const expressLayouts = require("express-ejs-layouts");
-const path = require("path");
+const bodyParser = require('body-parser')
 
-const indexRouter = require("./routes/index");
-const authorRouter = require("./routes/author")
-const bookRouter = require("./routes/books")
+const indexRouter = require('./routes/index')
+const authorRouter = require('./routes/authors')
+const bookRouter = require('./routes/books')
 
-app.set("view engine", "ejs");
-
-app.set("views", path.join(__dirname, "views"));
-app.set("layout", "layouts/layout");
-app.use(expressLayouts);
-app.use(express.static("public"))
-app.use(express.urlencoded({ extended: true }));
+app.set('view engine', 'ejs')
+app.set('views', __dirname + '/views')
+app.set('layout', 'layouts/layout')
+app.use(expressLayouts)
+app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }))
 
 const mongoose = require("mongoose");
 mongoose.connect(process.env.DATABASE_URL)
 
 const db = mongoose.connection
 db.on("error", error => console.error(error))
-db.once("open", error => console.log("Connected to Mongoose"))
+db.once("open", () => console.log("Connected to Mongoose"))
 
 app.use("/", indexRouter)
 app.use("/authors", authorRouter)
